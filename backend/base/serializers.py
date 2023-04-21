a5da9f456eb0ee805861a7a4578858d75d55682d
mvipe <mohnishvats@gmail.com>
2023-04-21 12:26:04 +0530

    completed backend user authentication

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,8 +1,45 @@
+from dataclasses import field
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Product
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class ProductsSerializers(serializers.ModelSerializer):
     class Meta:
         model=Product
         fields='__all__'
+
+class UserSerializers(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only=True)
+    _id = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model=User
+        fields=['id','_id','username','email','name','isAdmin']
+
+
+    def get_isAdmin(self,obj):
+        return obj.is_staff
+
+
+
+    def get__id(self,obj):
+        return obj.id
+    
+    def get_name(self,obj):
+        name=obj.first_name
+        if(name==''):
+            name=obj.email
+        return name
+
+
+class UserSerializerWithToken(UserSerializers):
+    token = serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model=User
+        fields=['id','_id','username','email','name','isAdmin','token']
+
+    def get_token(self,obj):
+        token=RefreshToken.for_user(obj)
+        return str(token.access_token)
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,14 +1,40 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.contrib.auth.models import User
 
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
 from setuptools import PEP420PackageFinder
 
 from .products import products
 from .models import Product
-from .serializers import ProductsSerializers
+from .serializers import ProductsSerializers, UserSerializers,UserSerializerWithToken
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from django.contrib.auth.hashers import make_password
+
+#from backend.base import serializers
+
+#from backend.base import serializers
 # Create your views here.
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+   def validate(self, attrs):
+        data = super().validate(attrs)
+
+        serializer= UserSerializerWithToken(self.user).data
+
+        for k,v in serializer.items():
+            data[k]=v
+
+
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class=MyTokenObtainPairSerializer
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -25,12 +51,51 @@ def getRoutes(request):
     ]
     return Response(routes)
 
+
+@api_view(['POST'])
+def registerUser(request):
+    data=request.data
+
+    user=User.objects.create(
+        first_name=data['name'],
+        username=data['email'],
+        email=data['email'],
+        password=make_password(data['password'])
+
+    )
+
+    serializer=UserSerializerWithToken(user,many=False)
+    
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getUserProfile(request):
+    user=request.user
+    
+    serializer=UserSerializers(user,many=False)
+
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getUsers(request):
+    users= User.objects.all()
+    serializer=UserSerializers(users,many=True)
+
+    return Response(serializer.data)
+
 @api_view(['GET'])
 def getProducts(request):
     products= Product.objects.all()
     serializer=ProductsSerializers(products,many=True)
 
     return Response(serializer.data)
+
+
+
 
 @api_view(['GET'])
 def getProduct(request,pk):
