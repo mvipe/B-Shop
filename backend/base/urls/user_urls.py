e0b7f7c40f47c8951c14adc5e01fc0eae7360138
mvipe <mohnishvats@gmail.com>
2023-04-22 21:54:30 +0530

    completed user profile update endpoint

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -6,6 +6,7 @@ from base.views import user_views as views
 urlpatterns = [
    
     path("profile/",views.getUserProfile,name="users-profile"),
+    path("profile/update/",views.updateUserProfile,name="user-profile-update"),
     path("",views.getUsers,name="users"),
 
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 
 from rest_framework.response import Response
-from rest_framework.decorators import api_view,permission_classes
-from rest_framework.permissions import IsAuthenticated,IsAdminUser
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from setuptools import PEP420PackageFinder
 
 
-from base.serializers import ProductsSerializers, UserSerializers,UserSerializerWithToken
+from base.serializers import ProductsSerializers, UserSerializers, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -16,20 +16,21 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth.hashers import make_password
 from rest_framework import status
 
+
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-   def validate(self, attrs):
+    def validate(self, attrs):
         data = super().validate(attrs)
 
-        serializer= UserSerializerWithToken(self.user).data
+        serializer = UserSerializerWithToken(self.user).data
 
-        for k,v in serializer.items():
-            data[k]=v
-
+        for k, v in serializer.items():
+            data[k] = v
 
         return data
 
+
 class MyTokenObtainPairView(TokenObtainPairView):
-    serializer_class=MyTokenObtainPairSerializer
+    serializer_class = MyTokenObtainPairSerializer
 
 # @api_view(['GET'])
 # def getRoutes(request):
@@ -49,10 +50,10 @@ class MyTokenObtainPairView(TokenObtainPairView):
 
 @api_view(['POST'])
 def registerUser(request):
-    data=request.data
+    data = request.data
 
     try:
-        user=User.objects.create(
+        user = User.objects.create(
             first_name=data['name'],
             username=data['email'],
             email=data['email'],
@@ -60,28 +61,48 @@ def registerUser(request):
 
         )
 
-        serializer=UserSerializerWithToken(user,many=False)
+        serializer = UserSerializerWithToken(user, many=False)
         return Response(serializer.data)
-    
+
     except:
-        message={'detail':'User with this email already exist.'}
-        return Response(message,status=status.HTTP_400_BAD_REQUEST)
+        message = {'detail': 'User with this email already exist.'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUserProfile(request):
+    user = request.user
+
+    serializer = UserSerializerWithToken(user, many=False)
+
+    data = request.data
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+
+    if data['password'] != '':
+        user.password = make_password(data['password'])
+
+    user.save()
+
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getUserProfile(request):
-    user=request.user
-    
-    serializer=UserSerializers(user,many=False)
+    user = request.user
+
+    serializer = UserSerializers(user, many=False)
 
     return Response(serializer.data)
+
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getUsers(request):
-    users= User.objects.all()
-    serializer=UserSerializers(users,many=True)
+    users = User.objects.all()
+    serializer = UserSerializers(users, many=True)
 
     return Response(serializer.data)
-
